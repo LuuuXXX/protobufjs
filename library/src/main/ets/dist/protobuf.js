447c1e6764e769e55ecaf2b047ba0abd3249fe66
liang_tao001 <liangtao50@h-partners.com>
2025-02-24 17:02:11 +0800

    支持BigInt类型功能
    Signed-off-by: liang_tao001 <liangtao50@h-partners.com>

--- library/src/main/ets/dist/protobuf.js
+++ library/src/main/ets/dist/protobuf.js
@@ -102,22 +102,36 @@ function requireLongbits () {
 	    return new LongBits(lo, hi);
 	};
 
+	LongBits.fromBigInt = function fromBigInt(value) {
+		if (value === 0n)
+			return zero;
+		const uint64 = BigInt.asUintN(64, value);
+		const lo = Number(uint64 & 0xFFFFFFFFn);
+		const hi = Number((uint64 >> 32n) & 0xFFFFFFFFn);
+		return new LongBits(lo >>> 0, hi >>> 0);
+	};
+
 	/**
 	 * Constructs new long bits from a number, long or string.
 	 * @param {Long|number|string} value Value
 	 * @returns {util.LongBits} Instance
 	 */
 	LongBits.from = function from(value) {
-	    if (typeof value === "number")
-	        return LongBits.fromNumber(value);
-	    if (util.isString(value)) {
-	        /* istanbul ignore else */
-	        if (util.Long)
-	            value = util.Long.fromString(value);
-	        else
-	            return LongBits.fromNumber(parseInt(value, 10));
-	    }
-	    return value.low || value.high ? new LongBits(value.low >>> 0, value.high >>> 0) : zero;
+		if (typeof value === "number") {
+			return LongBits.fromNumber(value);
+		}
+		if (typeof value === "bigint") {
+			return LongBits.fromBigInt(value);
+		}
+		if (util.isString(value)) {
+			/* istanbul ignore else */
+			if (util.Long) {
+				value = util.Long.fromString(value);
+			} else {
+				return LongBits.fromNumber(parseInt(value, 10));
+			}
+		}
+		return value.low || value.high ? new LongBits(value.low >>> 0, value.high >>> 0) : zero;
 	};
 
 	/**
@@ -134,6 +148,11 @@ function requireLongbits () {
 	        return -(lo + hi * 4294967296);
 	    }
 	    return this.lo + this.hi * 4294967296;
+	};
+
+	LongBits.prototype.toBigInt = function toBigInt(unsigned) {
+		const uint64 = (BigInt(this.hi) << 32n) | BigInt(this.lo);
+		return unsigned ? uint64 : BigInt.asIntN(64, uint64);
 	};
 
 	/**
@@ -417,6 +436,8 @@ function requireMinimal () {
 		         || /* istanbul ignore next */ util.global.Long
 		         || Long;
 
+		util.BigInt = util.global.dcodeIO && util.global.dcodeIO.BigInt || util.global.BigInt || BigInt;
+
 		/**
 		 * Regular expression used to verify 2 bit (`bool`) map keys.
 		 * @type {RegExp}
@@ -671,7 +692,7 @@ function requireMinimal () {
 		        function Buffer_allocUnsafe(size) {
 		            return new Buffer(size);
 		        };
-		}; 
+		};
 	} (minimal));
 	return minimal;
 }
@@ -1606,35 +1627,41 @@ Reader$1.prototype.skipType = function(wireType) {
     return this;
 };
 
-Reader$1._configure = function(BufferReader_) {
-    BufferReader$1 = BufferReader_;
-    Reader$1.create = create();
-    BufferReader$1._configure();
+Reader$1._configure = function (BufferReader_) {
+	BufferReader$1 = BufferReader_;
+	Reader$1.create = create();
+	BufferReader$1._configure();
+	let fn = '';
+	if (util$5.Long) {
+		fn = "toLong";
+	} else if (util$5.BigInt) {
+		fn = "toBigInt";
+	} else {
+		fn = "toNumber";
+	}
+	util$5.merge(Reader$1.prototype, {
 
-    var fn = util$5.Long ? "toLong" : /* istanbul ignore next */ "toNumber";
-    util$5.merge(Reader$1.prototype, {
+		int64: function read_int64() {
+			return readLongVarint.call(this)[fn](false);
+		},
 
-        int64: function read_int64() {
-            return readLongVarint.call(this)[fn](false);
-        },
+		uint64: function read_uint64() {
+			return readLongVarint.call(this)[fn](true);
+		},
 
-        uint64: function read_uint64() {
-            return readLongVarint.call(this)[fn](true);
-        },
+		sint64: function read_sint64() {
+			return readLongVarint.call(this).zzDecode()[fn](false);
+		},
 
-        sint64: function read_sint64() {
-            return readLongVarint.call(this).zzDecode()[fn](false);
-        },
+		fixed64: function read_fixed64() {
+			return readFixed64.call(this)[fn](true);
+		},
 
-        fixed64: function read_fixed64() {
-            return readFixed64.call(this)[fn](true);
-        },
+		sfixed64: function read_sfixed64() {
+			return readFixed64.call(this)[fn](false);
+		}
 
-        sfixed64: function read_sfixed64() {
-            return readFixed64.call(this)[fn](false);
-        }
-
-    });
+	});
 };
 
 var reader_buffer = BufferReader;
@@ -1867,7 +1894,7 @@ Service$1.prototype.end = function end(endedByRPC) {
 	 * @returns {undefined}
 	 */
 
-	rpc.Service = service$1; 
+	rpc.Service = service$1;
 } (rpc));
 
 var roots = {};
@@ -1907,7 +1934,7 @@ var roots = {};
 	}
 
 	// Set up buffer utility according to the environment
-	configure(); 
+	configure();
 } (indexMinimal));
 
 var util$2 = {exports: {}};
@@ -2114,7 +2141,7 @@ function requireTypes () {
 		    /* fixed64  */ 1,
 		    /* sfixed64 */ 1,
 		    /* bool     */ 0
-		]); 
+		]);
 	} (types$1));
 	return types$1;
 }
@@ -4399,7 +4426,7 @@ function requireConverter () {
 		    return gen
 		    ("return d");
 		    /* eslint-enable no-unexpected-multiline, block-scoped-var, no-redeclare */
-		}; 
+		};
 	} (converter));
 	return converter;
 }
@@ -4507,7 +4534,7 @@ var wrappers = {};
 
 	        return this.toObject(message, options);
 	    }
-	}; 
+	};
 } (wrappers));
 
 var type;
